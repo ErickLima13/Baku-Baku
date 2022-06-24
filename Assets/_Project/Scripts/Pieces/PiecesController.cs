0f000c3b4a44489e5fa563d83190c7f9be7dd1ae
Erick Lima <70773204+ErickLima13@users.noreply.github.com>
2022-06-24 16:25:49 -0300

    Touch Controls

--- Assets/_Project/Scripts/Pieces/PiecesController.cs
+++ Assets/_Project/Scripts/Pieces/PiecesController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
 public class PiecesController : MonoBehaviour
 {
@@ -12,6 +13,15 @@ public class PiecesController : MonoBehaviour
     private Spawner spawner;
 
     private List<BlockObject> blocks = new List<BlockObject>();
+
+    [Header("Touch")]
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+    private Vector2 endTouchPosition;
+    private bool stopTouch = false;
+
+    public float swipeRange;
+    public float tapRange;
 
     public void Initialize(Block animalBlock, Block foodBlock)
     {
@@ -44,14 +54,152 @@ public class PiecesController : MonoBehaviour
     {
         if (!gameManager.isPaused)
         {
-            Move();
+            //Move();
+            Swipe();
         }
+
         
     }
 
+    public void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+            timer = speed;
+        }
+
+        AutomaticFall();
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentPosition = Input.GetTouch(0).position;
+            Vector2 Distance = currentPosition - startTouchPosition;
+
+            if (!stopTouch)
+            {
+
+                if (Distance.x < -swipeRange)
+                {
+                    //outputText.text = "Left";
+                    stopTouch = true;
+
+                    timer += Time.deltaTime;
+
+                    if (timer > speed)
+                    {
+                        transform.position += Vector3.left;
+                        timer = 0;
+                    }
+
+                    if (ValidPosition())
+                    {
+                        gameManager.UpdateGrid(this);
+
+                    }
+                    else
+                    {
+                        transform.position += Vector3.right;
+                    }
+
+                }
+                else if (Distance.x > swipeRange)
+                {
+                    //outputText.text = "Right";
+                    stopTouch = true;
+
+                    timer += Time.deltaTime;
+
+                    if (timer > speed)
+                    {
+                        transform.position += Vector3.right;
+                        timer = 0;
+                    }
+
+                    if (ValidPosition())
+                    {
+                        gameManager.UpdateGrid(this);
+
+                    }
+                    else
+                    {
+                        transform.position += Vector3.left;
+                    }
+
+                }
+                else if (Distance.y > swipeRange)
+                {
+                    //outputText.text = "Up";
+                    stopTouch = true;
+                }
+                else if (Distance.y < -swipeRange)
+                {
+                    //outputText.text = "Down";
+                    stopTouch = true;
+
+                    timer += Time.deltaTime;
+
+                    if (timer > speed)
+                    {
+                        transform.position += Vector3.down;
+                        timer = 0;
+                    }
+
+                    if (ValidPosition())
+                    {
+                        gameManager.UpdateGrid(this);
+
+                    }
+                    else
+                    {
+                        transform.position += Vector3.up;
+                        enabled = false;
+                        spawner.SpawnPieces();
+                        AnimationFall();
+
+                        if (gameManager.AboveGrid(this))
+                        {
+                            gameManager.GameOver();
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            stopTouch = false;
+
+            endTouchPosition = Input.GetTouch(0).position;
+
+            Vector2 Distance = endTouchPosition - startTouchPosition;
+
+            if (Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange)
+            {
+                //outputText.text = "Tap";
+
+                transform.Rotate(0, 0, 90);
+
+                if (ValidPosition())
+                {
+                    gameManager.UpdateGrid(this);
+                }
+                else
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+            }
+
+        }
+
+
+    }
     private void Move()
     {
-        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow))
         {
             timer = speed;
         }
@@ -60,7 +208,7 @@ public class PiecesController : MonoBehaviour
         {
             timer += Time.deltaTime;
 
-            if(timer > speed)
+            if (timer > speed)
             {
                 transform.position += Vector3.right;
                 timer = 0;
@@ -69,7 +217,7 @@ public class PiecesController : MonoBehaviour
             if (ValidPosition())
             {
                 gameManager.UpdateGrid(this);
-               
+
             }
             else
             {
@@ -91,7 +239,7 @@ public class PiecesController : MonoBehaviour
             if (ValidPosition())
             {
                 gameManager.UpdateGrid(this);
-                
+
             }
             else
             {
@@ -100,7 +248,7 @@ public class PiecesController : MonoBehaviour
 
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)) 
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             timer += Time.deltaTime;
 
@@ -113,7 +261,7 @@ public class PiecesController : MonoBehaviour
             if (ValidPosition())
             {
                 gameManager.UpdateGrid(this);
-                
+
             }
             else
             {
@@ -146,18 +294,36 @@ public class PiecesController : MonoBehaviour
         }
     }
 
+   
+    //public void Rotate(InputAction.CallbackContext context)
+    //{
+    //    if (context.performed && !gameManager)
+    //    {
+    //        transform.Rotate(0, 0, 90);
+
+    //        if (ValidPosition())
+    //        {
+    //            gameManager.UpdateGrid(this);
+    //        }
+    //        else
+    //        {
+    //            transform.Rotate(0, 0, -90);
+    //        }
+    //    }
+    //}
+
     private void AnimationFall()
     {
-        for(int i = 0;i < blocks.Count; i++)
+        for (int i = 0; i < blocks.Count; i++)
         {
             blocks[i].GetComponent<SpriteRenderer>().sprite = blocks[i].myFrontSprite;
             blocks[i].GetComponent<Animator>().enabled = false;
-        }    
+        }
     }
 
     private void AutomaticFall()
     {
-        if (Time.time - fall >= (1/gameManager.difficulty) && !Input.GetKey(KeyCode.DownArrow))
+        if (Time.time - fall >= (1 / gameManager.difficulty) && !Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down;
             fall = Time.time;
@@ -181,13 +347,13 @@ public class PiecesController : MonoBehaviour
 
             }
 
-            
+
         }
     }
 
     private bool ValidPosition()
     {
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
             Vector2 posBlock = gameManager.RoundValue(child.position);
 
@@ -204,4 +370,5 @@ public class PiecesController : MonoBehaviour
 
         return true;
     }
+
 }
